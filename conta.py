f93f08f141f1c4c7a3b369ff7f47bce40060f2c5
Josenaldo de Oliveira Matos Filho <josenaldo@gmail.com>
2021-09-15 06:47:31 -0300

    Python 3: Introdução a Orientação a objetos Aula 03 Tópico 08

--- conta.py
+++ conta.py
@@ -1,2 +1,23 @@
+
+
 class Conta:
-    pass
+
+    def __init__(self, numero, titular, saldo, limite=1000.0):
+        print("Construindo objeto ... {}".format(self))
+        self.numero = numero
+        self.titular = titular
+        self.saldo = saldo
+        self.limite = limite
+
+    def extrato(self):
+        print("===========================================")
+        print(f"Número: {self.numero}")
+        print(f"Titular: {self.titular}")
+        print(f"Saldo: {self.saldo:.2f}")
+        print("===========================================")
+
+    def deposita(self, valor):
+        self.saldo += valor
+
+    def saca(self, valor):
+        self.saldo -= valor
