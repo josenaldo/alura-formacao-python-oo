9fd46e6fc1768698c87576b16cfedecb091f99be
Josenaldo de Oliveira Matos Filho <josenaldo@gmail.com>
2021-09-15 10:48:12 -0300

    Python 3: Introdução a Orientação a objetos Aula 05

--- conta.py
+++ conta.py
@@ -1,6 +1,5 @@
-
-
 class Conta:
+    banco = "Banco Qualquer Coisa"
 
     def __init__(self, numero, titular, saldo, limite=1000.0):
         print("Construindo objeto ... {}".format(self))
@@ -11,6 +10,7 @@ class Conta:
 
     def extrato(self):
         print("===========================================")
+        print(f"Banco: {Conta.banco}")
         print(f"Número: {self.__numero}")
         print(f"Titular: {self.__titular}")
         print(f"Saldo: {self.__saldo:.2f}")
@@ -20,8 +20,28 @@ class Conta:
         self.__saldo += valor
 
     def saca(self, valor):
+        if(self.saldo - valor < -self.limite):
+            print('Não é possível sacar abaixo do limite')
+            return
+
         self.__saldo -= valor
 
     def transfere(self, valor, destino):
         self.saca(valor)
         destino.deposita(valor)
+
+    @property
+    def saldo(self):
+        return self.__saldo
+
+    @property
+    def titular(self):
+        return self.__titular
+
+    @property
+    def limite(self):
+        return self.__limite
+
+    @limite.setter
+    def limite(self, limite):
+        self.__limite = limite
---
+++ main.py
@@ -0,0 +1,7 @@
+from conta import Conta
+
+c1 = Conta(123, "Nico", 55.5, 1000.0)
+c2 = Conta(145, "Pedro", 100.5, 1000.0)
+
+c1.extrato()
+c2.extrato()
